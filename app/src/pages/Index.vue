99e42dd93a7b52fef4b696a33cde9f395ad03aec
s-light <git@s-light.eu>
2020-12-02 12:02:53 +0100

    more experiments..

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,14 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="">
+      '{{ messages }}'
+    </div>
   </q-page>
 </template>
 
 <script>
+import api from '../api'
+
+const messages = api.service('messages')
+// Listen to user events
+messages.on('created', user => {
+  this.$data.messages = this.$data.messages.concat(user)
+})
+
 export default {
+  data () {
+    return {
+      test_message: 'Hello World'
+    }
+  },
   name: 'PageIndex'
 }
 </script>
